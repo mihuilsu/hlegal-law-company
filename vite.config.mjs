805d2b3ba4bc276a6f6fc0c3b8bd6835e8cb5f73
Michael Rimsky <czech.hudek@gmail.com>
2025-05-03 00:22:11 +0300

    updated/fixed vite.config.mjs

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,6 +1,17 @@
-import { defineConfig } from 'vite'
+import { defineConfig } from 'vite';
+import { resolve } from 'path';
 
 export default defineConfig({
   root: '.',
   base: './',
-})
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        publications: resolve(__dirname, 'publications.html'),
+        about: resolve(__dirname, 'about.html'),
+        team: resolve(__dirname, 'team.html')
+      },
+    },
+  },
+});
